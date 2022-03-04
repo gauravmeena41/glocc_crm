69ad91148d3bd7e1950a29de1ea0e7f96cbc84cc
gaurav <gurumeena41.gm@gmail.com>
2022-03-05 00:26:50 +0530

    added functions for changing user personal details, added functions for changing admin details and for setting new admin

--- contracts/CRM.sol
+++ contracts/CRM.sol
@@ -7,12 +7,22 @@ contract CRM {
     using Counters for Counters.Counter;
     Counters.Counter private _userCount;
 
+    string[] private teams = [
+        "Commerce-cloud",
+        "Marketing-cloud",
+        "Velocity-cloud",
+        "Health-cloud"
+    ];
+
     struct User {
         uint256 userId;
         address userAddress;
         string name;
         string email;
+        string avatar;
+        uint256 mobile;
         string role;
+        string team;
         bool isAdmin;
         uint256[] checkIn;
         uint256[] checkOut;
@@ -22,12 +32,20 @@ contract CRM {
 
     mapping(uint256 => User) public users;
 
-    constructor(string memory _name, string memory _email) {
+    constructor(
+        string memory _name,
+        string memory _email,
+        uint256 _mobile,
+        string memory _avatar
+    ) {
         admin.userId = 0;
         admin.userAddress = msg.sender;
         admin.name = _name;
         admin.email = _email;
+        admin.mobile = _mobile;
+        admin.avatar = _avatar;
         admin.role = "admin";
+        admin.team = "Management";
         admin.isAdmin = true;
     }
 
@@ -39,11 +57,34 @@ contract CRM {
         _;
     }
 
+    function changeAdmin(
+        string memory _name,
+        string memory _email,
+        uint256 _mobile
+    ) external onlyAdmin {
+        require(
+            msg.sender == admin.userAddress,
+            "Only current admin can set new admin"
+        );
+        admin.name = _name;
+        admin.email = _email;
+        admin.mobile = _mobile;
+    }
+
+    function changeAdminavatar(string memory _avatar) external onlyAdmin {
+        require(
+            msg.sender == admin.userAddress,
+            "Only current admin can set new avatar"
+        );
+        admin.avatar = _avatar;
+    }
+
     function addUser(
         string memory _name,
         string memory _email,
         bool _isAdmin,
-        string memory _role
+        string memory _role,
+        string memory _team
     ) external onlyAdmin {
         _userCount.increment();
         User storage user = users[_userCount.current()];
@@ -51,17 +92,18 @@ contract CRM {
         user.userAddress = msg.sender;
         user.name = _name;
         user.email = _email;
-        user.isAdmin = _isAdmin;
         user.role = _role;
+        user.team = _team;
+        user.isAdmin = _isAdmin;
     }
 
     function fetchUsers() external view returns (User[] memory) {
-        uint256 itemCount = _userCount.current();
+        uint256 userCount = _userCount.current();
         uint256 currentIndex = 0;
 
-        User[] memory result = new User[](itemCount);
+        User[] memory result = new User[](userCount);
 
-        for (uint256 i = itemCount; i > 0; i--) {
+        for (uint256 i = userCount; i > 0; i--) {
             User storage currentItem = users[i];
             result[currentIndex] = currentItem;
             currentIndex++;
@@ -76,6 +118,88 @@ contract CRM {
 
     function removeUser(uint256 _userId) external onlyAdmin {
         delete users[_userId];
+    }
+
+    function changeUsername(uint256 _userId, string memory _name) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+
+        require(
+            bytes(_name).length <= 32 && bytes(_name).length > 0,
+            "Username should have minimum 1 and maximum 32 charactor"
+        );
+
+        user.name = _name;
+    }
+
+    function changeUseremail(uint256 _userId, string memory _email) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+
+        require(
+            bytes(_email).length <= 32 && bytes(_email).length > 0,
+            "Email should be a valid email address"
+        );
+
+        user.email = _email;
+    }
+
+    function changeUseravatar(uint256 _userId, string memory _avatar) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+
+        require(
+            bytes(_avatar).length <= 32 && bytes(_avatar).length > 0,
+            "Your avatar isn't in a valid format"
+        );
+
+        user.avatar = _avatar;
+    }
+
+    function changeUsermobile(uint256 _userId, uint256 _mobile) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+        require(_mobile == 10, "Mobile number should be a valid mobile number");
+
+        user.mobile = _mobile;
+    }
+
+    function changeUserrole(uint256 _userId, string memory _role) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+
+        user.role = _role;
+    }
+
+    function changeUserteam(uint256 _userId, string memory _team) external {
+        User storage user = users[_userId];
+
+        require(
+            user.userAddress == msg.sender || user.isAdmin,
+            "Only owner can change username"
+        );
+
+        user.team = _team;
     }
 
     function checkIn(uint256 _userId) external {
--- test/test.js
+++ test/test.js
@@ -5,46 +5,62 @@ const moment = require("moment");
 describe("CRM", function () {
   it("Should create the CRM and admin", async function () {
     const Crm = await ethers.getContractFactory("CRM");
-    const crm = await Crm.deploy("Guru", "gurumeena41.gm@gmail.com");
+    const crm = await Crm.deploy(
+      "Guru",
+      "gurumeena41.gm@gmail.com",
+      8769973256,
+      "guru.png"
+    );
     await crm.deployed();
 
     console.log("CRM deployed to: ", crm.address);
 
+    // console.log(admin.mobile.toNumber());
+
+    await crm.changeAdminavatar("admin.png");
     const admin = await crm.admin();
 
-    // console.log(admin);
+    console.log(admin);
+
+    // await crm.addUser("guru", "guru@gmail.com", false, "Blockchain Developer");
+    // await crm.addUser("Guru", "guru@gmail.com", false, "Blockchain Developer");
+
+    // await crm.checkIn(1);
+    // await crm.checkOut(1);
+    // await crm.checkIn(1);
+    // await crm.checkOut(1);
+    // await crm.checkIn(1);
+    // await crm.checkOut(1);
+
+    // let users = await crm.fetchUsers();
+
+    // const searchUser = await crm.searchUser(1);
+    // console.log(users);
+
+    // await crm.removeUser(2);
+    // console.log("Deleted");
+
+    // users = await crm.fetchUsers();
+
+    // for (let i = 0; i < users.length; i++) {
+    //   if (
+    //     users[i].userAddress !== "0x0000000000000000000000000000000000000000"
+    //   ) {
+    //     console.log(users[i].name);
+    //   }
+    // }
 
     await crm.addUser(
       "Gaurav Meena",
       "garauvmeena@gmail.com",
       false,
-      "Developer"
+      "Developer",
+      "Commerce-Cloud"
     );
 
-    // await crm.addUser(
-    //   "Gaurav",
-    //   "garauv@gmail.com",
-    //   false,
-    //   "Blockchain Developer"
-    // );
+    // await crm.changeUserteam(1, "Commerce Cloud");
 
-    await crm.checkIn(1);
-    await crm.checkOut(1);
-    await crm.checkIn(1);
-    await crm.checkOut(1);
-    await crm.checkIn(1);
-    await crm.checkOut(1);
-
-    let users = await crm.fetchUsers();
-
-    // const searchUser = await crm.searchUser(1);
-    console.log(users);
-
-    await crm.removeUser(1);
-    console.log("Deleted");
-
-    users = await crm.fetchUsers();
-
-    console.log(users);
+    // const user = await crm.searchUser(1);
+    // console.log(user);
   });
 });
