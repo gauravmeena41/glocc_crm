02cb825ef765f496cbed4726fb7f06761ec86001
gurumeena41 <gurumeena41.gm@gmail.com>
2022-03-23 20:31:59 +0530

    removed code from smart contract

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const gllocAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-    export const gllocToken = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const gllocAddress = "0x62B7Bc20E6427Ae86fE16644D9096ca9e056528D";
+    export const gllocToken = "0x7AD4Aa6271f7843257abBd97c0BBbA0102Bba482";
